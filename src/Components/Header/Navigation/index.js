9763e1dbd2892ac2bc10f52dd77d8b3b4fa0af8e
ifaz <ifaz26522@gmail.com>
2025-09-17 18:53:55 +0600

    add: home Products are added

--- src/Components/Header/Navigation/index.js
+++ src/Components/Header/Navigation/index.js
@@ -1,6 +1,7 @@
 import Button from '@mui/material/Button';
 import { IoIosMenu } from "react-icons/io";
 import { FaAngleDown } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     return (
@@ -8,13 +9,22 @@ const Navigation = () => {
             <div className='container'>
                 <div className='col-sm-3 navPart1'>
                     <Button className='allCatTab align-items-center'>
-                        <span><IoIosMenu/></span>
+                        <span className='icon1 mr-2'><IoIosMenu/></span>
                         <span class="text">ALL CATEGORIES</span>
-                        <span><FaAngleDown/></span>
+                        <span className='icon2 ml-2'><FaAngleDown/></span>
                     </Button>
                 </div>
 
                 <div className='col-sm-9 navPart2'>
+                    <ul className='list list-inline'>
+                        <li className='list-inline-item'><Link to="/">Home</Link></li>
+                        <li className='list-inline-item'><Link to="/">FASHION</Link></li>
+                        <li className='list-inline-item'><Link to="/">ELECTRONIC</Link></li>
+                        <li className='list-inline-item'><Link to="/">BAKERY</Link></li>
+                        <li className='list-inline-item'><Link to="/">GROCERY</Link></li>
+                        <li className='list-inline-item'><Link to="/">BLOG</Link></li>
+                        <li className='list-inline-item'><Link to="/">CONTACT</Link></li>
+                    </ul>
                 </div>
             </div>
         </nav>
