c8067e70dc16d88ac0fa39dfa7be79e43178e40f
ifaz <ifaz26522@gmail.com>
2025-10-09 16:06:27 +0600

    feat: enhance ProductDetails page with pricing information and product rating

--- src/Pages/ProductDetails/index.js
+++ src/Pages/ProductDetails/index.js
@@ -1,4 +1,5 @@
 import ProductZoom from "../../Components/ProductZoom";
+import Rating from "@mui/material/Rating";
 
 const ProductDetails = () => {
   return (
@@ -7,19 +8,39 @@ const ProductDetails = () => {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-                <ProductZoom />
+              <ProductZoom />
             </div>
-            
+
             <div className="col-md-7">
-                <h2 className="hd text-capitalize">Good Life Kachi Ghani Mustard Oil 1 L (Pouch)</h2>
-                <ul className="list list-inline">
-                    <li className="list inline-item">
-                        <div className="d-flex align-items-center">
-                            <span className="text-light me-2">Brands : </span>
-                            <span>Wlich's</span>
-                        </div>
-                    </li>
-                </ul>
+              <h2 className="hd text-capitalize">
+                Good Life Kachi Ghani Mustard Oil 1 L (Pouch)
+              </h2>
+              <ul className="list list-inline d-flex align-items-center">
+                <li className="list inline-item">
+                  <div className="d-flex align-items-center">
+                    <span className="text-light me-2">Brands : </span>
+                    <span>Welch's </span>
+                  </div>
+                </li>
+
+                <li className="list-inline-item">
+                  <div className="d-flex align-items-center">
+                    <Rating
+                      name="read-only"
+                      value={4.5}
+                      precision={0.5}
+                      readOnly
+                      size="small"
+                    />
+                    <span className="text-light cursor ms-2">1 Review</span>
+                  </div>
+                </li>
+              </ul>
+
+              <div class="d-flex info">
+                <span class="oldPrice me-2">$20.00</span>
+                <span class="netPrice text-danger">$14.00</span>
+              </div>
             </div>
           </div>
         </div>
